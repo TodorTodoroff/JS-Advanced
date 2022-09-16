af4d2e936b48468e4f5f9f9b6aed3d76d1cbb29f
Todor Todorov <78209454+TodorTodoroff@users.noreply.github.com>
2022-09-16 12:51:10 +0300

    add some problems

---
+++ Arrays and Nested Arrays/Exercise/PrintEveryNthElementOfAnArray02.js
@@ -0,0 +1,35 @@
+function solve(arr, step){
+let result = [];
+
+for(let i = 0; i < arr.length; i+= step){
+    result.push(arr[i]);
+}
+
+return result;
+
+}
+
+
+console.log(solve(['5', 
+'20', 
+'31', 
+'4', 
+'20'], 
+2
+));
+
+console.log(solve(['dsa',
+'asd', 
+'test', 
+'tset'], 
+2
+));
+
+
+console.log(solve(['1', 
+'2',
+'3', 
+'4', 
+'5'], 
+6
+));
