839d6b23097097c3f0c4d45c84de66ed89401135
Todor Todorov <78209454+TodorTodoroff@users.noreply.github.com>
2022-09-18 21:22:41 +0300

    add more problems

---
+++ 02.Arrays and Nested Arrays/Exercise/SortArrayByTwoCriteria08.js
@@ -0,0 +1,29 @@
+function solve(input) {
+    input.sort(twoCriteriaSort);
+    input.forEach(el => console.log(el));
+
+    function twoCriteriaSort(cur, next) {
+        if (cur.length === next.length) {
+            return cur.localeCompare(next);
+        }
+        return cur.length - next.length;
+    }
+}
+
+solve(['alpha',
+    'beta',
+    'gamma']
+);
+
+solve(['Isacc',
+    'Theodor',
+    'Jack',
+    'Harrison',
+    'George']
+);
+
+solve(['test',
+    'Deny',
+    'omen',
+    'Default']
+);
--- 02.Arrays and Nested Arrays/Exercise/SortingNumber07.js
+++ 02.Arrays and Nested Arrays/Exercise/SortingNumber07.js
@@ -1,9 +1,14 @@
 function solve(arr) {
-let result = [];
+    let result = [];
 
-arr.reduce
+    arr.sort((a, b) => a - b);
 
-    return arr;
+    while (arr.length > 0) {
+        result.push(arr.shift());
+        result.push(arr.pop());
+    }
+
+    return result;
 }
 
 console.log(solve([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]));
