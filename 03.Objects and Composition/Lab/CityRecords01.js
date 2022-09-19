cae3ea7ca14abc9505218aebb16982ea3acb164d
Todor Todorov <78209454+TodorTodoroff@users.noreply.github.com>
2022-09-19 19:13:07 +0300

    new problems solved

---
+++ 03.Objects and Composition/Lab/CityRecords01.js
@@ -0,0 +1,19 @@
+function solve(name, population, treasury){
+
+    return city = {
+        name,
+        population,
+        treasury
+    };
+
+}
+
+console.log(solve('Tortuga',
+7000,
+15000
+));
+
+console.log(solve('Santo Domingo',
+12000,
+23500
+));
