fb6e44d9750b352e29e171a0e9d9540782cec4c3
Todor Todorov <78209454+TodorTodoroff@users.noreply.github.com>
2022-09-19 20:06:03 +0300

    new problems solved

---
+++ 03.Objects and Composition/Lab/CityTaxes03.js
@@ -0,0 +1,18 @@
+function solve(name, population, treasury) {
+    return city = {
+        name,
+        population,
+        treasury,
+        taxRate: 10,
+        collectTaxes() {
+            this.treasury += Math.floor(this.population * this.taxRate);
+        },
+        applyGrowth(percentage) {
+            this.population += Math.floor(this.population * percentage / 100);
+        },
+        applyRecession(percentage) {
+            this.treasury -= Math.floor(this.treasury * percentage / 100);
+        }
+    };
+
+}
