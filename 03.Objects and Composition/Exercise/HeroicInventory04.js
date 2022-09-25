b3bddd640e5122d323b9f6042a3ce2995db1a4c9
Todor Todorov <78209454+TodorTodoroff@users.noreply.github.com>
2022-09-25 13:13:29 +0300

    new problems solved

--- 03.Objects and Composition/Exercise/HeroicInventory04.js
+++ 03.Objects and Composition/Exercise/HeroicInventory04.js
@@ -1,3 +1,25 @@
-function solve() {
+function solve(heroes) {
+    let result = [];
 
+    for (let i = 0; i < heroes.length; i++) {
+        let obj = {};
+        let split = heroes[i].split(' / ');
+        obj['name'] = split[0];
+        obj['level'] = Number(split[1]);
+        obj['items'] = split[2].split(', ');
+        result[i] = obj;
+    }
+
+    return JSON.stringify(result);
 }
+
+let output = solve(['Isacc / 25 / Apple, GravityGun',
+    'Derek / 12 / BarrelVest, DestructionSword',
+    'Hes / 1 / Desolator, Sentinel, Antara']
+);
+
+console.log(output);
+
+output = solve(['Jake / 1000 / Gauss, HolidayGrenade']);
+
+console.log(output);
