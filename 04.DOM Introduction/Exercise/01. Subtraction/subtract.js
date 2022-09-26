01847773340742617cca5ba3ebfe09e034864aac
Todor Todorov <78209454+TodorTodoroff@users.noreply.github.com>
2022-09-26 18:20:18 +0300

    add project structure

--- 04.DOM Introduction/Exercise/01. Subtraction/subtract.js
+++ 04.DOM Introduction/Exercise/01. Subtraction/subtract.js
@@ -1,3 +1,8 @@
 function subtract() {
-    console.log('TODO:...');
-}
+    let firstNumber = document.getElementById('firstNumber').value;
+    let secondNumber = document.getElementById('secondNumber').value;
+  
+    let result = Number(firstNumber) - Number(secondNumber);
+  
+    document.getElementById('result').textContent = result;
+  }
