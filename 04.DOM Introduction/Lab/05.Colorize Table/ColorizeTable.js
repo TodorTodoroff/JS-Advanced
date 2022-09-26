fb8b31bcc37faf91e8b31be93207ca419e9cf3f6
Todor Todorov <78209454+TodorTodoroff@users.noreply.github.com>
2022-09-26 18:10:47 +0300

    new problems solved

--- 04.DOM Introduction/Lab/05.Colorize Table/ColorizeTable.js
+++ 04.DOM Introduction/Lab/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,8 @@
 function colorize() {
-    // TODO
+    let elements = Array.from(document.getElementsByTagName('tr'));
+
+    for (let i = 1; i < elements.length; i += 2) {
+        let element = elements[i];
+        element.style.backgroundColor = 'teal';
+    }
 }
