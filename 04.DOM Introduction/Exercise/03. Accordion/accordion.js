c3051eb0df283d11293d51c69c2b24f18f5334ea
Todor Todorov <78209454+TodorTodoroff@users.noreply.github.com>
2022-10-04 17:03:21 +0300

    new problems solved

--- 04.DOM Introduction/Exercise/03. Accordion/accordion.js
+++ 04.DOM Introduction/Exercise/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
-}
+    let extraContent = document.getElementById('extra');
+    let button = document.getElementsByClassName('button')[0];
+  
+    if (button.textContent == 'More') {
+      extraContent.style.display = 'block';
+      button.textContent = 'Less';
+    } else {
+      extraContent.style.display = 'none';
+      button.textContent = 'More';
+    }
+  }
