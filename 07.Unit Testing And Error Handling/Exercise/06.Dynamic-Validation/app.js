e4e7c643f1a30b3f8463d2688d186db6e22dbfa1
Todor Todorov <78209454+TodorTodoroff@users.noreply.github.com>
2022-10-09 13:54:46 +0300

    solved new problems

--- 07.Unit Testing And Error Handling/Exercise/06.Dynamic-Validation/app.js
+++ 07.Unit Testing And Error Handling/Exercise/06.Dynamic-Validation/app.js
@@ -1,3 +1,14 @@
 function validate() {
-    // TODO
+    let input = document.getElementById("email");
+    input.addEventListener("change", onChange);
+
+    function onChange(e) {
+        let email = e.target.value;
+        let validEmailPattern = /^[a-z]+@[a-z]+\.[a-z]+/g;
+        if (validEmailPattern.test(email)) {
+            e.target.classList.remove("error");
+        } else {
+            e.target.classList.add("error");
+        }
+    }
 }
