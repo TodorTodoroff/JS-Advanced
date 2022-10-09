f5207f8fb19b3512e9074e9edaec60c098c64da4
Todor Todorov <78209454+TodorTodoroff@users.noreply.github.com>
2022-10-09 12:51:44 +0300

    solved new problems

--- 07.Unit Testing And Error Handling/Exercise/test/isLookUpChar03.test.js
+++ 07.Unit Testing And Error Handling/Exercise/test/isLookUpChar03.test.js
@@ -4,7 +4,7 @@ let { lookupChar } = require("../CharLookUp03");
 
 describe("test function lookupChar exits", () => {
     it("test undefined output with integer and string as an argument", () => {
-        assert.equal(lookupChar(2, "test"), undefined);
+        assert.equal(lookupChar(2, 'test'), undefined);
     });
     it("test undefined output with integer and integer as an argument", () => {
         assert.equal(lookupChar(2, 2), undefined);
@@ -12,19 +12,25 @@ describe("test function lookupChar exits", () => {
     it("test undefined output with string and integer as an argument", () => {
         assert.equal(lookupChar('test', '2'), undefined);
     });
+    it("test undefined output with string and integer as an argument", () => {
+        assert.equal(lookupChar('test', 2.5), undefined);
+    });
     it("test undefined output with obj and array as an argument", () => {
         assert.equal(lookupChar({}, []), undefined);
     });
     it("test Incorrect index output with string and integer as an argument", () => {
-        assert.equal(lookupChar("tod", 3), "Incorrect index");
+        assert.equal(lookupChar('tod', 3), 'Incorrect index');
     });
     it("test Incorrect index output with string and integer as an argument", () => {
-        assert.equal(lookupChar("tod", 4), "Incorrect index");
+        assert.equal(lookupChar('tod', 4), 'Incorrect index');
     });
     it("test Incorrect index output with string and integer as an argument", () => {
-        assert.equal(lookupChar("tod", -1), "Incorrect index");
+        assert.equal(lookupChar('tod', -1), 'Incorrect index');
     });
     it("test correct output with string and integer as an argument", () => {
-        assert.equal(lookupChar("tod", 1), "o");
+        assert.equal(lookupChar('tod', 1), 'o');
+    });
+    it("test correct output with string and integer as an argument", () => {
+        assert.notEqual(lookupChar('tod', 2), 'o');
     });
 });
