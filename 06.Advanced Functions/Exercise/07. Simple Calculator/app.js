8272d52344e38aede4f3570377794d00d8e2b497
Todor Todorov <78209454+TodorTodoroff@users.noreply.github.com>
2022-10-09 12:35:11 +0300

    solved new problems

--- 06.Advanced Functions/Exercise/07. Simple Calculator/app.js
+++ 06.Advanced Functions/Exercise/07. Simple Calculator/app.js
@@ -1,5 +1,15 @@
 function calculator() {
-    // TODO:
+    return {
+        init: (selector1, selector2, resSelector) => {
+            this.s1 = document.querySelector(selector1);
+            this.s2 = document.querySelector(selector2);
+            this.resS = document.querySelector(resSelector);
+        },
+        add: () =>
+            (this.resS.value = Number(this.s1.value) + Number(this.s2.value)),
+        subtract: () =>
+            (this.resS.value = Number(this.s1.value) - Number(this.s2.value)),
+    };
 }
 
 
--- 06.Advanced Functions/Exercise/08. Next Article/script.js
+++ 06.Advanced Functions/Exercise/08. Next Article/script.js
@@ -1,3 +1,12 @@
 function getArticleGenerator(articles) {
-    // TODO
+    let div = document.getElementById('content');
+
+    return function showNext() {
+        if (articles.length) {
+            let articleText = articles.shift();
+            let article = document.createElement('article');
+            article.textContent = articleText;
+            div.appendChild(article);
+        }
+    };
 }
