176df8251dbb029686371912d6050ddd1b7fc77e
Todor Todorov <78209454+TodorTodoroff@users.noreply.github.com>
2022-11-01 09:51:05 +0200

    add new problems

---
+++ 08.Classes/Exercise/AutoEngineeringCompany09.js
@@ -0,0 +1,18 @@
+function solve(arr) {
+
+
+
+}
+
+solve([
+    'Audi | Q7 | 1000',
+    'Audi | Q6 | 100',
+    'BMW | X5 | 1000',
+    'BMW | X6 | 100',
+    'Citroen | C4 | 123',
+    'Volga | GAZ-24 | 1000000',
+    'Lada | Niva | 1000000',
+    'Lada | Jigula | 1000000',
+    'Citroen | C4 | 22',
+    'Citroen | C5 | 10'
+]);
