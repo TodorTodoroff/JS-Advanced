fa785524b4921ab007627e7705adfa0e1c667f5b
Todor Todorov <78209454+TodorTodoroff@users.noreply.github.com>
2022-11-02 10:38:08 +0200

    solved new problem

--- 09.Async Programming/Exercise/02.Bus-Schedule/app.js
+++ 09.Async Programming/Exercise/02.Bus-Schedule/app.js
@@ -1,16 +1,37 @@
 function solve() {
+    const infoText = document.querySelector('.info');
+    const departBtn = document.querySelector('#depart');
+    const arriveBtn = document.querySelector('#arrive');
+
+    let busStop = {
+        next: 'depot',
+    };
 
     function depart() {
-        console.log('Depart TODO...');
+        departBtn.disabled = true;
+        arriveBtn.disabled = false;
+
+        fetch(`http://localhost:3030/jsonstore/bus/schedule/${busStop.next}`)
+            .then((response) => response.json())
+            .then((data) => {
+                busStop = Object.assign(data);
+                infoText.textContent = `Next stop ${busStop.name}`;
+            })
+            .catch(() => {
+                infoText.textContent = 'Error!';
+            });
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        arriveBtn.disabled = true;
+        departBtn.disabled = false;
+
+        infoText.textContent = `Arriving at ${busStop.name}`;
     }
 
     return {
         depart,
-        arrive
+        arrive,
     };
 }
 
