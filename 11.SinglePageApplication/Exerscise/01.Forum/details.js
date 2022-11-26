8ef77ed96bc933620ebb4ed98299b7ef609d7c6d
Todor Todorov <78209454+TodorTodoroff@users.noreply.github.com>
2022-11-26 16:22:46 +0200

    working on details section

--- 11.SinglePageApplication/Exerscise/01.Forum/details.js
+++ 11.SinglePageApplication/Exerscise/01.Forum/details.js
@@ -3,6 +3,7 @@ const section = document.getElementById("detailsView");
 const main = document.getElementsByTagName("main")[0];
 const form = section.querySelector("form");
 form.addEventListener("submit", onSubmit);
+const themeContentWrapper = document.getElementById("theme-content-wrapper");
 let id;
 
 section.remove();
@@ -21,13 +22,54 @@ export async function showDetails(e) {
     const topic = await loadTopic(id);
     const comments = await loadComment(id);
 
-    const result = topicTemplate(topic, comments); //tbd
-    // section.replaceChildren(result);
+    const result = topicTemplate(topic, comments);
+    themeContentWrapper.replaceChildren(result);
     main.replaceChildren(section);
 }
 
 function topicTemplate(topic, comments) {
+    const topicContainer = document.createElement("div");
+    topicContainer.classList.add("theme-title");
+    topicContainer.innerHTML = `
+            <div class="theme-name-wrapper">
+                <div class="theme-name">
+                 <h2>${topic.topicName}</h2>
+                </div>
+            </div>`;
 
+    const commentsContainer = document.createElement("div");
+    commentsContainer.classList.add("comment");
+    commentsContainer.innerHTML = `
+            <div class="header">
+                <img src="./static/profile.png" alt="avatar">
+                <p><span>${topic.username}</span> posted on <time>${topic.date}</time></p>
+
+                 <p class="post-content">${topic.postText}</p>
+            </div>`;
+
+    const commentElements = Object.values(comments).forEach(com => {
+       const comment = createComment(com);
+       commentsContainer.appendChild(comment);
+    });
+    
+
+    return commentsContainer;
+}
+
+function createComment(data) {
+    const container = document.createElement("div");
+    container.classList.add("user-comment");
+    container.innerHTML = `
+    <div class="topic-name-wrapper">
+        <div class="topic-name">
+            <p><strong>${data.username}</strong> commented on <time>${data.date}</time></p>
+            <div class="post-content">
+            <p>${data.postText}</p>
+            </div>
+        </div>
+    </div>`;
+
+    return container;
 }
 
 
@@ -35,7 +77,7 @@ function onSubmit(e) {
     e.preventDefault();
     const formData = new FormData(form);
     const { postText, username } = Object.fromEntries(formData);
-    createPost({ postText, username, id });
+    createPost({ postText, username, id, date: new Date() });
 
 
 }
