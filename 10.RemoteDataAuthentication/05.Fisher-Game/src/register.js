002bf60cad5b072f291a00aca7d908507a45f127
Todor Todorov <78209454+TodorTodoroff@users.noreply.github.com>
2022-11-26 10:37:46 +0200

    implemented register functiality on Fisher-Game

--- 10.RemoteDataAuthentication/05.Fisher-Game/src/register.js
+++ 10.RemoteDataAuthentication/05.Fisher-Game/src/register.js
@@ -1,1 +1,64 @@
-console.log('TODO:// Implement Register functionality');
+document.getElementById("register-form").addEventListener("submit", registerHandler);
+document.querySelectorAll("a").forEach(el => el.classList.remove("active"));
+document.getElementById("register").classList.add("active");
+const errorP = document.querySelector("p.notification");
+
+function registerHandler(e) {
+
+    // TODO validation
+    e.preventDefault();
+
+    const form = e.target;
+    const formData = new FormData(form);
+    const { email, password, rePassword } = Object.fromEntries(formData);
+
+    if (password !== rePassword) {
+        errorP.textContent = "Error";
+        setTimeout(() => {
+            errorP.textContent = "";
+        }, 2000);
+    }
+
+    onRegister(email, password);
+}
+
+
+async function onRegister(email, password) {
+
+    //TODO error handling       
+    const url = `http://localhost:3030/users/register`;
+
+
+    const body = {
+        email,
+        password
+    };
+
+    try {
+        const header = getHeader("POST", body);
+        const response = await fetch(url, header);
+        const data = await response.json();
+
+        if(data.code !== 200){
+            throw new Error(data.message)
+        }
+
+        return data;
+    } catch (e) {
+        errorP.textContent = e;
+        setTimeout(()=> {
+            errorP.textContent = ""
+        }, 2000);
+    }
+
+}
+
+function getHeader(method, body) {
+    return {
+        method: `${method}`,
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify(body)
+    }
+}
