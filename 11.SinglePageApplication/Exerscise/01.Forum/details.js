ee0ed8d115a91fee34720fdb96012a9911a39e9d
Todor Todorov <78209454+TodorTodoroff@users.noreply.github.com>
2022-11-26 15:34:11 +0200

    working on details section

--- 11.SinglePageApplication/Exerscise/01.Forum/details.js
+++ 11.SinglePageApplication/Exerscise/01.Forum/details.js
@@ -1,29 +1,71 @@
 
 const section = document.getElementById("detailsView");
 const main = document.getElementsByTagName("main")[0];
+const form = section.querySelector("form");
+form.addEventListener("submit", onSubmit);
+let id;
 
 section.remove();
 
 export async function showDetails(e) {
-    let id;
-    if (e.target.tagName === 'H2') {
+    if (!e) {
+        return;
+    }
+
+    if (e.target.tagName === "H2") {
         id = e.target.parentElement.id;
-    } else {
+    } else if (e.target.tagName === "A") {
         id = e.target.id;
     }
 
-    const topic = loadTopic(id);
-    // const comments = loadComment(id);
-    // const result = topicTemplate(topic, comments); //tbd
+    const topic = await loadTopic(id);
+    const comments = await loadComment(id);
+
+    const result = topicTemplate(topic, comments); //tbd
     // section.replaceChildren(result);
     main.replaceChildren(section);
 }
 
-function topicTemplate(topic, comments){
+function topicTemplate(topic, comments) {
 
 }
 
-async function loadTopic(id){
+
+function onSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const { postText, username } = Object.fromEntries(formData);
+    createPost({ postText, username, id });
+
+
+}
+
+async function createPost(body) {
+    const url = `http://localhost:3030/jsonstore/collections/myboard/comments`;
+
+    const response = await fetch(url, {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(body)
+    });
+
+    const data = await response.json();
+    clearForm();
+}
+
+function clearForm() {
+    form.reset();
+}
+
+async function loadComment(id) {
+    const url = `http://localhost:3030/jsonstore/collections/myboard/comments`;
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return data;
+}
+
+async function loadTopic(id) {
     const url = `http://localhost:3030/jsonstore/collections/myboard/posts/${id}`;
 
     const response = await fetch(url);
