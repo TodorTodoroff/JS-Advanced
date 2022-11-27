dd46cfa6b64b4d5c82a822ae3cc22313515d20c2
Todor Todorov <78209454+TodorTodoroff@users.noreply.github.com>
2022-11-27 11:54:37 +0200

    resolved a problem

---
+++ 13.ClientSideRendering/01.List-Towns/app.js
@@ -0,0 +1,34 @@
+import {html, render} from './node_modules/lit-html/lit-html.js'
+
+const form = document.querySelector("form");
+form.addEventListener("submit", onSubmit);
+const root = document.getElementById("root");
+
+function onSubmit(e){
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    const {towns} = Object.fromEntries(formData);
+    const townsArray = towns.split(", ");
+    
+    renderTownList(townsArray);
+    form.reset();    
+}
+
+function renderTownList(data){
+    const result = createTownList(data);
+    render(result, root);
+
+}
+
+
+function createTownList(data){
+    const ul = html`
+        <ul>
+            ${data.map( x =>html`<li>${x}</li>`)}
+        </ul>
+    `;
+
+    return ul;
+}
+
