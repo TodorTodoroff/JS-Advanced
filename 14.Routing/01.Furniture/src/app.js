7d9f9f7fc875e57eb10c1cacd0073a81065bf821
Todor Todorov <78209454+TodorTodoroff@users.noreply.github.com>
2022-11-28 09:40:52 +0200

    create view infrastracure

---
+++ 14.Routing/01.Furniture/src/app.js
@@ -0,0 +1,11 @@
+import page from "../node_modules/page/page.mjs"
+
+page("/");
+page("/catalog");
+page("/create");
+page("/details/id");
+page("/edit/id");
+page("/login");
+page("/register");
+page("/my-furniture");
+
