20edaf49349a1f4d926501be3ed400afa8f09113
Todor Todorov <78209454+TodorTodoroff@users.noreply.github.com>
2022-11-29 16:41:03 +0200

    implement login

--- 15.ExamPreparation/01.PetCareExam/src/app.js
+++ 15.ExamPreparation/01.PetCareExam/src/app.js
@@ -1,6 +1,7 @@
 import { page, render } from './lib.js'
 import { showCatalog } from './views/catalog.js';
 import { showHome } from './views/home.js';
+import { showLogin } from './views/login.js';
 
 const main = document.getElementById('content');
 
@@ -10,7 +11,7 @@ page('/catalog', showCatalog);
 page('/catalog/:id', () => console.log("details"));
 page('/edit/:id', () => console.log("edit"));
 page('/create', () => console.log("create"));
-page('/login', () => console.log("login"));
+page('/login', showLogin);
 page('/register', () => console.log("register"));
 
 
