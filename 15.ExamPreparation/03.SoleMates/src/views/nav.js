87a8c23851c9e66999539db9204b052476180565
Todor Todorov <78209454+TodorTodoroff@users.noreply.github.com>
2022-12-01 20:39:53 +0200

    implemented details and delete

--- 15.ExamPreparation/03.SoleMates/src/views/nav.js
+++ 15.ExamPreparation/03.SoleMates/src/views/nav.js
@@ -6,27 +6,25 @@ import { getUserData } from '../util.js';
 const nav = document.querySelector('header');
 
 const navTemplate = (hasUser) => html`
-        <a id="logo" href="/"
-          ><img id="logo-img" src="./images/logo.png" alt=""
-        /></a>
-
+        <a id="logo" href="/"><img id="logo-img" src="./images/logo.png" alt="" /></a>
+        
         <nav>
-          <div>
-            <a href="/catalog">Dashboard</a>
-            <a href="/search">Search</a>
-          </div>
-          <!-- Logged-in users -->
-          ${hasUser ? html`
-          <div class="user">
-            <a href="/create">Add Pair</a>
-            <a href="javascript:void(0)" @click=${onLogout}>Logout</a>
-          </div>`
+            <div>
+                <a href="/catalog">Dashboard</a>
+                <a href="/search">Search</a>
+            </div>
+            <!-- Logged-in users -->
+            ${hasUser ? html`
+            <div class="user">
+                <a href="/create">Add Pair</a>
+                <a href="javascript:void(0)" @click=${onLogout}>Logout</a>
+            </div>`
           : html`
-          <!-- Guest users -->
-          <div class="guest">
-            <a href="/login">Login</a>
-            <a href="/register">Register</a>
-          </div>`}
+            <!-- Guest users -->
+            <div class="guest">
+                <a href="/login">Login</a>
+                <a href="/register">Register</a>
+            </div>`}
         </nav>`;
 
 export function updateNav() {
