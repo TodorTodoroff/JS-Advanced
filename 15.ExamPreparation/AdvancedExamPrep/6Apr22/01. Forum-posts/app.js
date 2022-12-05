a0ec9e0675a902b8f76c9e744442176fb6b1c4d4
Todor Todorov <78209454+TodorTodoroff@users.noreply.github.com>
2022-12-05 18:36:57 +0200

    resolved problem

--- 15.ExamPreparation/AdvancedExamPrep/6Apr22/01. Forum-posts/app.js
+++ 15.ExamPreparation/AdvancedExamPrep/6Apr22/01. Forum-posts/app.js
@@ -1,103 +1,92 @@
 window.addEventListener("load", solve);
 
 function solve() {
-    document.getElementById("publish-btn").addEventListener("click", createPost);
-    document.getElementById("clear-btn").addEventListener("click", clearPublishedList);
+    let reviewEl = document.getElementById("review-list");
+    let publishEl = document.getElementById("published-list");
+    document.getElementById("clear-btn").addEventListener("click", onClear);
 
-    let title = document.getElementById("post-title");
-    let category = document.getElementById("post-category");
-    let content = document.getElementById("post-content");
-    let reviewSection = document.getElementById("review-list");
-    let approveSection = document.getElementById("published-list");
+    let titleEl = document.getElementById("post-title");
+    let categoryEl = document.getElementById("post-category");
+    let contentEl = document.getElementById("post-content");
 
-    function createPost(event) {
-        let titleValue = title.value;
-        let categoryValue = category.value;
-        let contentValue = content.value;
+    document.getElementById("publish-btn").addEventListener("click", onSubmit);
 
-        if (!titleValue || !categoryValue || !contentValue) {
+    function onSubmit() {
+        let titleV = titleEl.value;
+        let categoryV = categoryEl.value;
+        let contentV = contentEl.value;
+
+        if (!titleV || !categoryV || !contentV) {
             return;
         };
 
-        createDOMElements(titleValue, categoryValue, contentValue);
-        clearFormFields();
+        createHTMLElements(titleV, categoryV, contentV);
+        clearFields();
     }
 
-    function clearFormFields() {
-        title.value = "";
-        category.value = "";
-        content.value = "";
-    }
 
-    function createDOMElements(title, category, content) {
-
+    function createHTMLElements(title, category, content) {
         let li = document.createElement("li");
         li.classList.add("rpost");
 
-        let article = createArticle(title, category, content);
+        let articleE = document.createElement("article");
+
+        let h4E = document.createElement("h4");
+        h4E.textContent = title;
+
+        let pCat = document.createElement("p");
+        pCat.textContent = `Category: ${category}`;
+
+        let pCont = document.createElement("p");
+        pCont.textContent = `Content: ${content}`;
 
         let editBtn = document.createElement("button");
         editBtn.classList.add("action-btn");
         editBtn.classList.add("edit");
         editBtn.textContent = "Edit";
-        editBtn.addEventListener("click", editPost);
+        editBtn.addEventListener("click", onEdit);
 
         let approveBtn = document.createElement("button");
         approveBtn.classList.add("action-btn");
         approveBtn.classList.add("approve");
         approveBtn.textContent = "Approve";
-        approveBtn.addEventListener("click", approvePost);
+        approveBtn.addEventListener("click", onApprove);
 
-        li.appendChild(article);
+        articleE.appendChild(h4E);
+        articleE.appendChild(pCat);
+        articleE.appendChild(pCont);
+        li.appendChild(articleE);
         li.appendChild(editBtn);
         li.appendChild(approveBtn);
 
-        reviewSection.appendChild(li);
-
+        reviewEl.appendChild(li);
     }
 
-    function createArticle(title, category, content) {
-        let article = document.createElement("article");
-        let h = document.createElement("h4");
-        h.textContent = title;
+    function onApprove(e) {
+        let rpost = e.target.parentElement;
+        Array.from(rpost.querySelectorAll("button")).forEach(x => x.remove());
 
-        let categoryP = document.createElement("p");
-        categoryP.textContent = `Category: ${category}`;
-
-        let contentP = document.createElement("p");
-        contentP.textContent = `Content: ${content}`;
-
-        article.appendChild(h);
-        article.appendChild(categoryP);
-        article.appendChild(contentP);
-
-        return article;
+        publishEl.appendChild(rpost);
     }
 
-    function editPost(e) {
+    function onEdit(e) {
         let currentPost = e.target.parentElement;
-        let articleContent = currentPost.getElementsByTagName("article")[0].children;
+        let articleChildren = currentPost.children[0].children;
 
-        let titleValue = articleContent[0].textContent;
-        let categoryValue = articleContent[1].textContent;
-        let contentValue = articleContent[2].textContent;
-
-        title.value = titleValue;
-        category.value = categoryValue.split(": ")[1];
-        content.value = contentValue.split(": ")[1];
+        titleEl.value = articleChildren[0].textContent;
+        categoryEl.value = articleChildren[1].textContent.split(": ")[1];
+        contentEl.value = articleChildren[2].textContent.split(": ")[1];
 
         currentPost.remove();
     }
 
-    function approvePost(e) {
-        let currentPost = e.target.parentElement;
-        Array.from(currentPost.querySelectorAll("button")).forEach(button => button.remove());
-        approveSection.appendChild(currentPost);
+    function onClear(e){
+        Array.from(publishEl.children).forEach(x => x.remove());
     }
 
-    function clearPublishedList(e) {
-        Array.from(approveSection.children).forEach(c => c.remove());
+    function clearFields() {
+        titleEl.value = "";
+        categoryEl.value = "";
+        contentEl.value = "";
     }
 }
-
-
